caa9285d80f54a553f53b6aadfbc51e1985dd8b8
Nicklas Utgaard <nutgaard@gmail.com>
2021-03-16 09:56:35 +0100

    [OPP-1331] setter riktige cookie config ved lokalt direkte kjøring

--- login-app/src/test/kotlin/no/nav/modialogin/MainTest.kt
+++ login-app/src/test/kotlin/no/nav/modialogin/MainTest.kt
@@ -6,6 +6,8 @@ fun main() {
     System.setProperty("IDP_DISCOVERY_URL", "http://localhost:8080/.well-known/openid-configuration")
     System.setProperty("IDP_CLIENT_ID", "foo")
     System.setProperty("IDP_CLIENT_SECRET", "bar")
+    System.setProperty("AUTH_TOKEN_RESOLVER", "modia_ID_token")
+    System.setProperty("REFRESH_TOKEN_RESOLVER", "modia_refresh_token")
     System.setProperty("EXPOSED_PORT", "8082")
     startApplication()
 }
