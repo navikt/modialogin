3daaefe9384182cae495bce3ad97d32f7b033bdb
Nicklas Utgaard <nutgaard@gmail.com>
2022-07-18 10:34:58 +0200

    [KAIZEN-0] doble netty's maks header størrelse

--- common/src/main/kotlin/no/nav/modialogin/common/KtorServer.kt
+++ common/src/main/kotlin/no/nav/modialogin/common/KtorServer.kt
@@ -3,6 +3,8 @@ package no.nav.modialogin.common
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.netty.handler.codec.http.HttpObjectDecoder
+import io.netty.handler.codec.http.HttpServerCodec
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -12,7 +14,19 @@ object KtorServer {
 
     fun server(port: Int, module: Application.(NaisState) -> Unit) {
         val naisState = NaisState()
-        val server = embeddedServer(Netty, port) {
+        val server = embeddedServer(Netty, port, configure = {
+            this.httpServerCodec = {
+                /**
+                 * Netty har som default 8kb maks på headere, med mange og store cookies kan man overskride dette.
+                 * Vi dobler derfor til 16kb
+                 */
+                HttpServerCodec(
+                    HttpObjectDecoder.DEFAULT_MAX_INITIAL_LINE_LENGTH,
+                    HttpObjectDecoder.DEFAULT_MAX_HEADER_SIZE * 2,
+                    HttpObjectDecoder.DEFAULT_MAX_CHUNK_SIZE,
+                )
+            }
+        }) {
             module(naisState)
             naisState.isReady = true
         }
